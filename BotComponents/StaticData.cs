ed2e099a157a266019d40dfb7e8fb679a9190162
Michael Müller <Michael.Helmut.Mueller@gmx.de>
2021-10-13 15:54:14 +0200

    language support for non-ui version, language files not in source code, but will be added in v1.1 release

--- BotComponents/StaticData.cs
+++ BotComponents/StaticData.cs
@@ -1,8 +1,6 @@
 ﻿using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace BotComponents
 {
@@ -10,7 +8,7 @@ namespace BotComponents
     {
         public static Dictionary<string, string> GetLanguageTokens(string language)
         {
-            var json = File.ReadAllText("C:/Users/Micha/Desktop/Darkymos Projects/Github/Scripts/Bots/BotComponents" + $"/Languages/{language}.json");
+            var json = File.ReadAllText(Directory.GetCurrentDirectory() + $"/Languages/{language}.json");
             return JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
         }
     }
