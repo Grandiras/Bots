08e2c1f94a9796f626ececc66d72ae1c582f1a08
Grandiras <Michael.Helmut.Mueller@gmx.de>
2023-02-27 23:10:40 +0100

    ...

---
+++ 10Bot/Services/PrivateVoiceManager.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace TenBot.Services;
+public class PrivateVoiceManager : IService
+{
+    private readonly ServerService ServerService;
+
+
+    public PrivateVoiceManager(ServerService serverService) => ServerService = serverService;
+
+
+    public SocketRole? GetPrivateChannelRoleAsync(SocketVoiceChannel voiceChannel)
+    {
+        var permission = voiceChannel.PermissionOverwrites.Where(x => x.Permissions.ViewChannel == PermValue.Allow);
+
+        return permission is IEnumerable<Overwrite> overwrites and not null && overwrites.Any()
+            ? ServerService.GetServer(voiceChannel.Guild.Id).Roles.First(x => x.Id == overwrites.First().TargetId)
+            : null;
+    }
+}
