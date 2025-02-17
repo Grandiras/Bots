48c10c9f81f7d864b2e65037d7249a2378349a15
Grandiras <Michael.Helmut.Mueller@gmx.de>
2025-02-17 18:47:33 +0100

    Enhance Discord bot setup in Program.cs
    Updated service configurations, added exception handling,
    OpenAPI documentation, and initialized required services.
    Set bot status and modified application run logic to
    include default endpoints.

--- TenBot/Program.cs
+++ TenBot/Program.cs
@@ -9,14 +9,17 @@ using TenBot.StandardFeatures;
 
 var builder = WebApplication.CreateBuilder();
 
+// Add defaults
 builder.AddServiceDefaults();
 builder.Services.AddProblemDetails();
 builder.Services.AddOpenApi();
 
+// Add secrets configuration
 builder.Services
     .AddSingleton(new SecretsConfiguration(builder.Configuration))
     .AddSingleton(builder.Configuration.GetSection("Bot").Get<BotConfiguration>()!);
 
+// Add Discord configuration and services
 builder.Services
     .AddSingleton<DiscordSocketClient>()
     .AddSingleton(new DiscordSocketConfig() { GatewayIntents = (GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent) & ~GatewayIntents.GuildScheduledEvents & ~GatewayIntents.GuildInvites })
@@ -24,6 +27,7 @@ builder.Services
     .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>(), x.GetRequiredService<InteractionServiceConfig>()))
     .AddSingleton(new InteractionServiceConfig() { UseCompiledLambda = true });
 
+// Add features and services flagged with the appropriate interfaces
 builder.Services.Scan(scan => scan.FromCallingAssembly()
     .AddClasses(classes => classes.AssignableTo<IService>())
         .AsSelf()
@@ -37,9 +41,11 @@ builder.Services.Scan(scan => scan.FromCallingAssembly()
 
 var app = builder.Build();
 
+// Add exception handling and OpenAPI documentation
 app.UseExceptionHandler();
 if (app.Environment.IsDevelopment()) _ = app.MapOpenApi();
 
+// Add logging and Discord event handlers
 var client = app.Services.GetRequiredService<DiscordSocketClient>();
 var configurator = app.Services.GetRequiredService<BotConfigurator>();
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
@@ -54,13 +60,16 @@ client.Ready += async () =>
     foreach (var service in app.Services.GetAllServicesWith<IMustPostInitialize>()) await service.PostInitializeAsync();
 };
 
+// Initialize services that require it
 foreach (var service in app.Services.GetAllServicesWith<IMustInitialize>()) _ = service.InitializeAsync();
 
+// Start the bot
 await client.LoginAsync(TokenType.Bot, configurator.GetToken());
 await client.StartAsync();
 
+// Set the bot's status
 await client.SetCustomStatusAsync("Use /help for more information!");
 
+// Add the default endpoints and run the application
 app.MapDefaultEndpoints();
-
 app.Run();
