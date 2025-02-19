8e7f99a87065641d73a147d11cfe0dcf90922289
Grandiras <Michael.Helmut.Mueller@gmx.de>
2025-02-19 15:49:55 +0100

    Refactor QuotesCommand to use FollowupAsync
    Replaced `RespondAsync` calls with `FollowupAsync` in multiple methods to enhance interaction flow by deferring responses. Refactored `ListAsync` to use a block body for improved readability and included `DeferAsync` for better response handling.

--- TenBot/Features/Quotes/QuotesCommand.cs
+++ TenBot/Features/Quotes/QuotesCommand.cs
@@ -12,9 +12,11 @@ public sealed class QuotesCommand(QuotesService QuotesService) : InteractionModu
                                   [Summary("display", "Determines, whether this quote should be displayed after creation or not.")] bool display = true,
                                   [Summary("context", "Additional information for this quote.")] string? context = null)
     {
+        await DeferAsync(!display);
+
         _ = QuotesService.AddQuote(new(quote, author, context), Context.ServerID);
 
-        await RespondAsync(embed: new EmbedBuilder()
+        _ = await FollowupAsync(embed: new EmbedBuilder()
             .WithTitle("New Quote")
             .WithDescription(quote)
             .WithColor(QuotesService.Feature.Color)
@@ -28,28 +30,32 @@ public sealed class QuotesCommand(QuotesService QuotesService) : InteractionModu
     [SlashCommand("delete", "Deletes a quote.")]
     public async Task DeleteAsync([Summary("quote", "The quote to delete."), Autocomplete(typeof(QuotesAutoCompleteHandler))] string quote)
     {
+        await DeferAsync(ephemeral: true);
+
         if (quote is null || (await QuotesService.GetQuote(Guid.Parse(quote), Context.ServerID)).IsT1)
         {
-            await RespondAsync("Quote not found.", ephemeral: true);
+            _ = await FollowupAsync("Quote not found.", ephemeral: true);
             return;
         }
 
         await QuotesService.RemoveQuote(Guid.Parse(quote), Context.ServerID);
-        await RespondAsync("Quote has successfully been deleted.", ephemeral: true);
+        _ = await FollowupAsync("Quote has successfully been deleted.", ephemeral: true);
     }
 
     [SlashCommand("display", "Prints a random quote.")]
     public async Task DisplayAsync([Summary("display", "Determines, whether this quote should be displayed to everyone or not.")] bool display = true)
     {
+        await DeferAsync(!display);
+
         var quote = await QuotesService.GetRandomQuote(Context.Guild.Id);
 
         if (quote.IsT1)
         {
-            await RespondAsync("No quotes found.", ephemeral: true);
+            _ = await FollowupAsync("No quotes found.", ephemeral: true);
             return;
         }
 
-        await RespondAsync(embed: new EmbedBuilder()
+        _ = await FollowupAsync(embed: new EmbedBuilder()
             .WithTitle("Quote")
             .WithDescription(quote.AsT0.Quote)
             .WithColor(QuotesService.Feature.Color)
@@ -65,26 +71,31 @@ public sealed class QuotesCommand(QuotesService QuotesService) : InteractionModu
 
     [SlashCommand("list", "Lists all available quotes.")]
     public async Task ListAsync()
-        => await RespondAsync(embed: new EmbedBuilder()
-        .WithTitle("Quotes")
-        .WithColor(QuotesService.Feature.Color)
-        .WithFields((await QuotesService.GetQuotes(Context.Guild.Id)).Select(x => new EmbedFieldBuilder()
-            .WithName(x.Quote)
-            .WithValue(x.Author + (x.Context is not null ? $", {x.Context}" : ""))))
-        .Build(), ephemeral: true);
+    {
+        await DeferAsync(ephemeral: true);
+        _ = await FollowupAsync(embed: new EmbedBuilder()
+            .WithTitle("Quotes")
+            .WithColor(QuotesService.Feature.Color)
+            .WithFields((await QuotesService.GetQuotes(Context.Guild.Id)).Select(x => new EmbedFieldBuilder()
+                .WithName(x.Quote)
+                .WithValue(x.Author + (x.Context is not null ? $", {x.Context}" : ""))))
+            .Build(), ephemeral: true);
+    }
 
     [MessageCommand("Search for a quote")]
     public async Task SearchAsync([Summary("message", "The message to search for.")] IMessage message)
     {
+        await DeferAsync();
+
         var quote = await QuotesService.GetMatchingQuote(message.Content, Context.Guild.Id);
 
         if (quote.IsT1)
         {
-            await RespondAsync("No matching quote found!", ephemeral: true);
+            _ = await FollowupAsync("No matching quote found!", ephemeral: true);
             return;
         }
 
-        await RespondAsync(embed: new EmbedBuilder()
+        _ = await FollowupAsync(embed: new EmbedBuilder()
             .WithTitle("Quote")
             .WithDescription(quote.AsT0.Quote)
             .WithColor(QuotesService.Feature.Color)
